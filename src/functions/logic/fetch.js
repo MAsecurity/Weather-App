29ae146964a99c7c7e55f49ab2e89432b9235db2
MAsecurity <108958617+MAsecurity@users.noreply.github.com>
2024-07-11 09:17:24 +0100

    Add api functionality

---
+++ src/functions/logic/fetch.js
@@ -0,0 +1,23 @@
+async function fetchRequest(input){
+  const validationBox = document.querySelector("#validation");
+  try{
+    let apiKey = '8f7034bc98884d48bb875750241107'
+    let response = await fetch(`https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${input}`,{mode:'cors'})
+    let weatherData = await response.json();
+    if(weatherData.error){
+      validationBox.classList.add('active');
+      validationBox.textContent = `You entered an invalid city (${input})`
+    }else{
+      if(validationBox.classList.contains('active')){
+        validationBox.classList.remove("active");
+        validationBox.textContent = '';
+      }else{
+        
+      }
+    }
+    console.log(weatherData);
+  }catch(error){
+    console.log(error)
+  }
+}
+export {fetchRequest}
